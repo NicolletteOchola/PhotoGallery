eb534026b5146877fa8acb7279852a3dd8fa72bb
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-31 09:57:02 +0300

    Added a category class for the category of the images and the methods for it.

--- imagelib/models.py
+++ imagelib/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Location(self):
+class Location(models.Model):
 
   location = models.CharField(max_length=255, blank =True)
 
@@ -23,3 +23,24 @@ class Location(self):
     verbose_name = 'Location'
     verbose_name_plural = 'Locations'
 
+class Category(models.Model):
+  category = models.CharField(max_length=255)
+
+  def save_category(self):
+    self.save()
+
+  def delete_category(self):
+    self.delete()
+
+  @classmethod
+  def search_by_category(cls, search_term):
+    images = cls.objects.filter(category__icontains=search_term)
+    return images
+  
+  def __str__(self):
+    return self.category
+
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+    
