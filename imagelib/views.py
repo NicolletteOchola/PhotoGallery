c9b9d32e495d48bc96f6af00669859327c68203e
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-31 09:46:55 +0300

    Added a location class and the class methods that go with it

--- imagelib/views.py
+++ imagelib/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,render_to_response
+import datetime as dt
 
 # Create your views here.
+def index(request):
+  date = dt.date.today()
+  images =
