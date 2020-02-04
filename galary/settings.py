b92b92a206c294f1241fd222f41465b74418291b
Nicole Ocholla <nicoleochola@gmail.com>
2020-02-04 11:46:59 +0300

    deployment attempt

--- galary/settings.py
+++ galary/settings.py
@@ -15,10 +15,6 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,30 +80,16 @@ WSGI_APPLICATION = 'galary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 # development
-if config('MODE')=="dev":
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'galary',
             'USER': 'nicole',
             'PASSWORD':'   ',
-            'HOST': config('DB_HOST'),
-            'PORT': '',
     }
 }
 
-# production
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=config(DATABASE_URL)
-        )
-    }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
